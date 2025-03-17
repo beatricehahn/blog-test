5473783b87c2051f5e64c09ff35798380878a71c
Beatrice Hahn <beatricehahn7@gmail.com>
2025-03-17 12:46:49 -0700

    Patch input method and returning values

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read a blog, "p" to create a post, or "q" to quit:'
+
 blogs = dict()
 
 def menu():
@@ -7,6 +9,7 @@ def menu():
     # Exit
 
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items(): # [(blog_name, Blog), (blog_name, Blog)]
